2960e0c5d7cf9738d8d05357326f871d49edf576
Илья Вобленко <ilya.voblenko@gmail.com>
2021-11-08 17:02:02 +0300

    fix

--- models/stonksModel.py
+++ models/stonksModel.py
@@ -1,24 +1,32 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-import seaborn as sns
 from prophet import Prophet
 from prophet.plot import add_changepoints_to_plot
 
 
-class StonksPrediction:
-    def __init__(self, company_ticket: str, api_key: str = "ZRMG7N7CVNEFA2RY", data_to_fit=None,
+class StonksModel:
+    def __init__(self, company_ticket: str, api_key: str = "ZRMG7N7CVNEFA2RY", model_type: str = "prophet",
                  train_test_split_scale: float = 0.8):
-        self.prophet = None
+
         self.train_test_split_scale = train_test_split_scale
-        self._data_to_fit = data_to_fit
-        self.company_ticket = company_ticket
         self.api_key = api_key
+        self.company_ticket = company_ticket.upper()
+
+        # self._models = {"prophet": Prophet}
+
+        self.model = None
+
+        self._data_to_fit = None
+
         self.forecast = None
         self.forecast_period = None
+
+        ##metrix
         self.rmse = None
         self.mape = None
         self.mae = None
+
         self._get_data()
         self._preprocessing()
         self._train_test_split()
@@ -38,6 +46,18 @@ class StonksPrediction:
         split_index = int(self._data_to_fit.shape[0] * (1 - self.train_test_split_scale))
         self._train_data = self._data_to_fit[split_index:]
         self._test_data = self._data_to_fit[:split_index]
+        self.forecast_period = self._test_data.shape[0]
+
+    ##TODO: добавить просчет праздников
+    def _generate_future_dates(self, days: int):
+        future = self.model.make_future_dataframe(periods=days)
+        future['day'] = future['ds'].dt.weekday
+        future = future[future['day'] <= 4]
+        future = future[future['ds'] >= self._test_data.loc[0, "ds"]]
+        future = self.model.make_future_dataframe(periods=2 * days - future.shape[0])
+        future['day'] = future['ds'].dt.weekday
+        future = future[future['day'] <= 4]
+        return future
 
     def fit(self, n_changepoints: int = None, changepoint_prior_scale: float = None, changepoint_range: float = None):
         """
@@ -46,26 +66,66 @@ class StonksPrediction:
         :param days_slice:
         :return:
         """
-        self.prophet = Prophet(daily_seasonality=True, n_changepoints=n_changepoints,
-                               changepoint_prior_scale=changepoint_prior_scale,
-                               changepoint_range=changepoint_range)
-        self.prophet.fit(self._train_data)
+        self.model = Prophet(daily_seasonality=True, n_changepoints=n_changepoints,
+                             changepoint_prior_scale=changepoint_prior_scale,
+                             changepoint_range=changepoint_range)
+        self.model.fit(self._train_data)
 
-    def predict(self, date_period: int = 90):
+    def predict(self, days: int = None):
         """
         :param date_period:
         :return:
         """
-        ##TODO: сделать адекватный просчет горизонта прогноза и фильтрации дат от пропусков в данных для обучения
-        future = self.prophet.make_future_dataframe(periods=2 * date_period)
-        future['day'] = future['ds'].dt.weekday
-        future = future[future['day'] <= 4]
-        self.forecast_period = future.shape[0]
-        self.forecast = self.prophet.predict(future)
+
+        if days is None:
+            future = self._generate_future_dates(self.forecast_period)
+        else:
+            future = self._generate_future_dates(days)
+            self.forecast_period = future.shape[0]
+        self.forecast = self.model.predict(future)
 
         self.mape = self._get_mape()
         self.mae = self._get_mae()
         self.rmse = self._get_rmse()
+
+    def print_prophet_predict(self, add_changepoints: bool = False):
+        """
+        :param add_changepoints:
+        :return:
+        """
+        fig = self.model.plot(self.forecast)
+        if add_changepoints:
+            a = add_changepoints_to_plot(fig.gca(), self.model, self.forecast)
+
+    def print_predict_with_real_data(self):
+        """
+        :param real_data:
+        :param days_slice:
+        :return:
+        """
+        forecast = self.forecast[-self.forecast_period:].merge(self._test_data, on="ds", how="right")
+        plt.plot(forecast.ds, forecast.y, color="red")
+        plt.plot(forecast.ds, forecast.yhat, color="black")
+        plt.plot(forecast.ds, forecast.yhat_lower, color="blue")
+        plt.plot(forecast.ds, forecast.yhat_upper, color="blue")
+        plt.xticks(rotation=45, ha='right')
+        plt.show()
+
+    ##TODO: убрать костыль с начальной датой отсчет времени
+    def _get_rmse(self):
+        return np.sqrt(np.mean((self._test_data.y - self.forecast[-self.forecast_period:].yhat) ** 2))
+
+    def _get_mape(self):
+        return np.mean((self._test_data.y - self.forecast[-self.forecast_period:].yhat) / self._test_data.y) * 100
+
+    def _get_mae(self):
+        return np.mean(np.abs(self._test_data.y - self.forecast[-self.forecast_period:].yhat))
+
+    def _get_metrix_sum(self):
+        return self._get_mape() + self._get_rmse() + self._get_mae()
+
+    def get_metrix(self):
+        return "RMSE: " + str(self.rmse) + "\n" + "MAPE: " + str(self.mape) + "\n" + "MAE: " + str(self.mae)
 
     def _update_hyperparameters(self):
         ##TODO: дополнить список гиперпараметров и подумать об оптимизации процесса перебора
@@ -103,48 +163,3 @@ class StonksPrediction:
         self.predict()
         self.print_predict_with_real_data()
         print(self.get_metrix())
-
-    def print_prophet_predict(self, add_changepoints: bool = False):
-        """
-        :param add_changepoints:
-        :return:
-        """
-        fig = self.prophet.plot(self.forecast)
-        if add_changepoints:
-            a = add_changepoints_to_plot(fig.gca(), self.prophet, self.forecast)
-
-    def print_predict_with_real_data(self):
-        """
-        :param real_data:
-        :param days_slice:
-        :return:
-        """
-        forecast = self.forecast[-self.forecast_period:].merge(self._test_data, on="ds", how="right")
-        plt.plot(forecast.ds, forecast.y, color="red")
-        plt.plot(forecast.ds, forecast.yhat, color="black")
-        plt.plot(forecast.ds, forecast.yhat_lower, color="blue")
-        plt.plot(forecast.ds, forecast.yhat_upper, color="blue")
-        plt.xticks(rotation=45, ha='right')
-        plt.show()
-
-    ##TODO: убрать костыль с начальной датой отсчет времени
-    def _get_rmse(self):
-        return np.sqrt(np.mean((self._test_data.y - self.forecast[-self.forecast_period:self.forecast_period -
-                                                                                        self._test_data.shape[
-                                                                                            0]].yhat) ** 2))
-
-    def _get_mape(self):
-        return np.mean((self._test_data.y - self.forecast[-self.forecast_period:self.forecast_period -
-                                                                                self._test_data.shape[
-                                                                                    0]].yhat) / self._test_data.y) * 100
-
-    def _get_mae(self):
-        return np.mean(np.abs(self._test_data.y - self.forecast[-self.forecast_period:self.forecast_period -
-                                                                                      self._test_data.shape[
-                                                                                          0]].yhat))
-
-    def _get_metrix_sum(self):
-        return self._get_mape() + self._get_rmse() + self._get_mae()
-
-    def get_metrix(self):
-        return "RMSE: " + str(self.rmse) + "\n" + "MAPE: " + str(self.mape) + "\n" + "MAE: " + str(self.mae)
