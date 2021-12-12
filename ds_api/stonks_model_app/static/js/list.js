a4a579c361dc7fe75cf62e50daf5542c149951d6
Илья Вобленко <ilya.voblenko@gmail.com>
2021-12-12 15:38:58 +0300

    update pages

--- ds_api/stonks_model_app/static/js/list.js
+++ ds_api/stonks_model_app/static/js/list.js
@@ -11,12 +11,12 @@ $(document).ready(function () {
     });
 });
 
-function getStockDetail(tiker) {
+function getStockDetail(ticker) {
     $.ajax({
         url: "/detail",
         type: "GET",
         dataType: "json",
-        data: {tiker: tiker},
+        data: {ticker: ticker},
         success: function (data) {
             document.getElementById('detail').removeAttribute('hidden');
             document.getElementById('company_description').innerText = data['info'][0]['description']
@@ -24,27 +24,9 @@ function getStockDetail(tiker) {
             document.getElementById('industry').innerText = data['info'][0]['industry']
             document.getElementById('sector').innerText = data['info'][0]['sector']
             document.getElementById('dividents').innerText = data['info'][0]['lastDiv']
-            chartDraw(data['historical'])
+            graph_data = data['historical'];
+            console.log(graph_data)
         }
     });
 }
 
-
-function chartDraw(map) {
-    var table = anychart.data.table('date');
-    table.addData(map);
-
-    mapping = table.mapAs({x: 'date', value: 'close'});
-
-    var chart = anychart.stock();
-
-    var series = chart.plot(0).area(mapping);
-
-    var scrollerSeries = chart.scroller().area(table.mapAs({'value': 'value'}));
-
-    series.seriesType("column");
-
-    scrollerSeries.seriesType("column");
-
-    document.getElementById('graph')
-}
